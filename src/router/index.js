2991d872a885e6a09c5535725c33872c55f1f463
bftsai <tsai19911026@gmail.com>
2024-01-24 16:54:23 +0800

    add deploy.sh & edit router setting

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
+  linkActiveClass: 'active',
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: () => import('../views/HomeView.vue')
     },
     {
       path: '/about',
