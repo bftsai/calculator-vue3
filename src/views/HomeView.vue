8c645d74566a75098f61fd662960594495f5aa1a
bftsai <tsai19911026@gmail.com>
2024-01-27 11:09:33 +0800

    add rule: keep arithmetic when click num after had result

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -79,11 +79,13 @@ export default {
     },
     storeNum(e){
       const reg = new RegExp('^-?[\\d]{1,9}$');
-      reg.test(Number(this.str))? this.str += e.target.textContent : this.prompt = true;
       if(this.symbol === '='){
         this.symbol = '';
+        this.str = String(this.total);
+        this.counting = false;
         this.total = 0;
       }
+      reg.test(Number(this.str))? this.str += e.target.textContent : this.prompt = true;
     },
     del(){
       this.str.slice(0,-1) === '-' ? this.str = '0' : this.str = this.str.slice(0,-1);
